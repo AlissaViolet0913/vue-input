fe0576d87a29a76331a5b0a18c94e056de0f5b9b
Yuri_Minemura <yuri.minemura@rakus-partners.co.jp>
2024-04-10 18:07:19 +0900

    listComponent完成

---
+++ src/components/ListComponent.vue
@@ -0,0 +1,15 @@
+
+<script setup>
+const fruitsList = [
+    { id: 0, name: "リンゴ" },
+    { id: 1, name: "いちご" },
+    { id: 2, name: "バナナ" },
+];
+</script>
+
+
+<template>
+    <ul>
+        <li v-for="fruit in fruitsList" :key="fruit.id">{{ fruit.name }}</li>
+    </ul>
+</template>
